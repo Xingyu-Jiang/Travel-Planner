783e9bd4b14e88bf31f7fce3bf821c1bd09fe849
xiangliu1123 <1784676846.xl@gmail.com>
2022-12-11 03:31:17 -0500

    map add
    map add and it connects

--- GUI.py
+++ GUI.py
@@ -1,7 +1,14 @@
+import os
+import sys
 import tkinter as tk
 from tkinter import ttk
 from tkinter import *
+
+import tkintermapview
+from geopy import Nominatim
+
 import Graph as G
+
 
 # Graph
 
@@ -18,6 +25,8 @@ a.addEdge("Japan", "Russia", 100, 250)
 a.addEdge("Japan", "Canada", 1000, 200)
 
 
+
+
 # this is a function which returns the selected combo box item
 def getSelectedComboItemStart():
     return StartCity.get()
@@ -26,28 +35,42 @@ def getSelectedComboItemStart():
 def getSelectedComboItemEnd():
     return EndCity.get()
 
+def reset():
+    btnClickFunction()
 
 # When the button is clicked
 def btnClickFunction():
     if getSelectedComboItemStart() != 'From' or getSelectedComboItemEnd() != 'To':
-        if a.ShortestRoute(getSelectedComboItemStart(), getSelectedComboItemEnd()):
+        if a.ShortestRoute(getSelectedComboItemStart(), getSelectedComboItemEnd()) and a.CheapestRoute(getSelectedComboItemStart(), getSelectedComboItemEnd()):
             ShortestOutput["text"] = a.ShortestRoute(getSelectedComboItemStart(), getSelectedComboItemEnd())
+            CheapOutput["text"] = a.CheapestRoute(getSelectedComboItemStart(), getSelectedComboItemEnd())
+
+            startLocation = geolocator.geocode(getSelectedComboItemStart())
+            endLocation = geolocator.geocode(getSelectedComboItemEnd())
+
+            marker_1 = map_widget.set_marker(startLocation.latitude, startLocation.longitude,
+                                             text=getSelectedComboItemStart())
+            x= marker_1
+            marker_2 = map_widget.set_marker(endLocation.latitude, endLocation.longitude,
+                                             text=getSelectedComboItemEnd())
+            y = marker_2
+            path_1 = map_widget.set_path(
+                [marker_1.position, marker_2.position, (startLocation.latitude, startLocation.longitude),
+                 (endLocation.latitude, endLocation.longitude)])
+            p = path_1
         else:
             ShortestOutput["text"] = "Currently, no routes available from {0} to {1}".format(
                 getSelectedComboItemStart(),getSelectedComboItemEnd())
-
-        if a.CheapestRoute(getSelectedComboItemStart(), getSelectedComboItemEnd()):
-            CheapOutput["text"] = a.CheapestRoute(getSelectedComboItemStart(), getSelectedComboItemEnd())
-        else:
             CheapOutput["text"] = "Currently, no routes available from {0} to {1}".format(
                 getSelectedComboItemStart(),getSelectedComboItemEnd())
 
-    print('clicked')
+
+print('clicked')
 
 # Main window configuration
 root = Tk()
 root.eval('tk::PlaceWindow . center')
-root.geometry('615x240')
+root.geometry('650x700')
 root.configure(background='#F0F8FF')
 root.title('Travel Planner')
 
@@ -61,6 +84,16 @@ StartCity.current(0)
 EndCity = ttk.Combobox(root, justify='center', values=a.EndGenerateVertexList(), font=('arial', 12, 'normal'), width=15)
 EndCity.grid(column=2, row=0, sticky=tk.W, padx=20, pady=20)
 EndCity.current(0)
+
+# Initialize Nominatim API
+geolocator = Nominatim(user_agent="MyApp")
+
+# create map widget
+map_widget = tkintermapview.TkinterMapView(root, width=500, height=400, corner_radius=1)
+map_widget.place(x=90, y=250)
+
+map_widget.set_position(40.11527480314763, -75.11050323951898)  # Penn State Abington
+map_widget.set_zoom(2)
 
 # Legend
 Label(root, text='→', bg='#F0F8FF', font=('arial', 22, 'normal')).grid(column=1, row=0, padx=25, pady=10)
@@ -81,4 +114,7 @@ CheapOutput.grid(column=1, row=2, columnspan=4, padx=20, pady=20)
 # Search button
 Search = Button(root, text='Search Route', bg='#F0F8FF', font=('arial', 12, 'normal'), command=btnClickFunction)
 Search.grid(column=3, row=0, padx=5, pady=5)
+
+Reset = Button(root, text='Reset', bg='#F0F8FF', font=('arial', 12, 'normal'), command= reset)
+Reset.grid(column=3, row=2, padx=5, pady=5)
 root.mainloop()
